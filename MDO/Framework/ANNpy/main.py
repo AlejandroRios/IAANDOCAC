dc2fb45823252c631949321af0263a725821211e
AlejandroRios <aarc.88@gmail.com>
2019-05-23 16:32:35 -0300

    fisrt commit

---
+++ MDO/Framework/ANNpy/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+################################################################################
+# Load networks
+
+################################################################################
+# User inputs
+
+# Airfoil data
+
+# Wing planform
+
+# Flight condition
+Mach = 0.75
+altitude = 10500
+fixed_AoA = 0
+CLinput = 0.43
+
+################################d################################################
+# Execution
+
+# Calculate Sobiescki coefficients of root airfoil
+
+
+# Calculate Sobiescki coefficients of tip airfoil
+
+################################################################################
+# Loading inputs data
+
+# Specific class
+class input_nn:
+    Mach = Mach
+    altitude = altitude
+    AoA = AoA
+    AR = AR
